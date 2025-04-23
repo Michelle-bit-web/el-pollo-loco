870edb3ad60d619ffb99bece04fbffd3511163cb
Michelle <mpuschkarow@web.de>
2025-04-23 17:09:26 +0200

    method loadImage added to MoveableObject to load images

--- js/classes/moveable-object.class.js
+++ js/classes/moveable-object.class.js
@@ -3,6 +3,11 @@ class MoveableObject {
     y = 20;
     img;
 
+    loadImage(path){
+        this.img = new Image(); //alternative zu: doc.ElById('image') <img id="image">
+        this.img.src = path;
+    }
+
     moveRight(){
         console.log('Moving right');
     }
--- js/game.js
+++ js/game.js
@@ -7,5 +7,6 @@ function init(){
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
 
-    console.log('My character is:', world.character)
+    console.log('My character is:', world.character);
+    console.log('My first chicken is:', world.enemies[0])
 }
