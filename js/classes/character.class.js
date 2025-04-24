a8bfef6a4097e8d344224a94f6416ff741ce3d56
Michelle <mpuschkarow@web.de>
2025-04-24 17:16:59 +0200

    moves left & right for character

--- js/classes/character.class.js
+++ js/classes/character.class.js
@@ -1,5 +1,6 @@
 class Character extends MovableObject {
     height = 280;
+    speed = 15;
     IMAGES_WALKING = [
         '../assets/img/2_character_pepe/2_walk/W-21.png',
         '../assets/img/2_character_pepe/2_walk/W-22.png',
@@ -20,12 +21,21 @@ class Character extends MovableObject {
     animate(){
         setInterval(() =>{
             if(this.world.keyboard.RIGHT){
+                this.x += this.speed;
+            }
+            if(this.world.keyboard.LEFT){
+                this.x -= this.speed;
+            }
+        }, 1000 / 30);
+
+        setInterval(() =>{
+                if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT){
                 let i = this.currentImage % this.IMAGES_WALKING.length;
                 let path = this.IMAGES_WALKING[i];
                 this.img = this.imageCache[path];
                 this.currentImage ++;
-            }
-        }, 1000/ 6);
+                }
+            }, 50);
     }
 
     jump(){
