32575d0d23240742e79054ca117a1f79199db000
Michelle <mpuschkarow@web.de>
2025-04-24 10:41:00 +0200

    load images method to load load more character images & save them

--- js/classes/movable-object.class.js
+++ js/classes/movable-object.class.js
@@ -4,10 +4,19 @@ class MovableObject {
     img;
     height = 200;
     width = 170;
+    imageCache = {};
 
     loadImage(path){
         this.img = new Image(); //alternative zu: doc.ElById('image') <img id="image">
         this.img.src = path;
+    }
+
+    loadImages(arr){
+        arr.forEach(path =>{
+            let img = new Image();
+            img.src = path; 
+            this.imageCache[path] = path;
+        });
     }
 
     moveRight(){
