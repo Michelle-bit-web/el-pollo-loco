2dfc2c1a3cf092edf690f4fc8313668b94d35083
Michelle <mpuschkarow@web.de>
2025-04-25 10:42:05 +0200

    added class endboss & outsourced animation with function playAnimation

--- js/classes/chicken.class.js
+++ js/classes/chicken.class.js
@@ -17,10 +17,7 @@ class Chicken extends MovableObject {
 
     animate(){
         setInterval(() =>{
-        let i = this.currentImage % this.IMAGES_WALKING.length;
-        let path = this.IMAGES_WALKING[i];
-        this.img = this.imageCache[path];
-        this.currentImage ++;
+            this.playAnimation(this.IMAGES_WALKING);
         }, 1000/ 6);
         this.moveLeft();
     }
---
+++ js/classes/endboss.class.js
@@ -0,0 +1,32 @@
+class Endboss extends MovableObject{
+
+    IMAGES_WALKING =[
+        '../assets/img/4_enemie_boss_chicken/1_walk/G1.png',
+        '../assets/img/4_enemie_boss_chicken/1_walk/G2.png',
+        '../assets/img/4_enemie_boss_chicken/1_walk/G3.png',
+        '../assets/img/4_enemie_boss_chicken/1_walk/G4.png',
+        // '../assets/img/4_enemie_boss_chicken/1_walk/G5.png',
+        // '../assets/img/4_enemie_boss_chicken/1_walk/G6.png',
+        // '../assets/img/4_enemie_boss_chicken/1_walk/G7.png',
+        // '../assets/img/4_enemie_boss_chicken/1_walk/G8.png',
+        // '../assets/img/4_enemie_boss_chicken/1_walk/G9.png',
+        // '../assets/img/4_enemie_boss_chicken/1_walk/G10.png',
+        // '../assets/img/4_enemie_boss_chicken/1_walk/G11.png',
+        // '../assets/img/4_enemie_boss_chicken/1_walk/G12.png',
+    ];
+
+    constructor(){
+        super().loadImage('../assets/img/4_enemie_boss_chicken/1_walk/G1.png');
+        this.x = 700;
+        this.loadImages(this.IMAGES_WALKING);
+        this.speed = 0.4 
+        this.animate();
+    }
+    
+    animate(){
+        setInterval(() =>{
+            this.playAnimation(this.IMAGES_WALKING);
+        }, 1000/ 6);
+        this.moveLeft();
+    }
+}
